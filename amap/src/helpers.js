eddc2bb6892d6d079d9c22acd6b51c71a968a939
Sadok386 <sadok.benfredj@hotmail.com>
2018-02-11 22:32:56 +0100

    fixed error

--- amap/src/helpers.js
+++ amap/src/helpers.js
@@ -1,6 +1,6 @@
 export function formatPrice(cents) {
   return `${(cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')} EUR`;
-
+}
 
 export function rando(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
